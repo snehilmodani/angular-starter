6746a5050d2e2fa50af84f9e8bd49da9a59d15f4
Snehil Modani <snehil@actonate.com>
2014-11-17 14:59:12 +0530

    controller logic example implemented.

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,50 @@ starterApp.controller('NotesController', function($scope, $timeout){
 
     }, 2000);
 });
+var clear = function(){
+        alert($scope.message);
+        $scope.message="";
+    };
+starterApp.controller('TextLimitController', function($scope){
+    $scope.MAX_LENGHT=100;
+    $scope.message="Hello World";
+    
+    $scope.remaining = function(){
+        //console.log($scope.MAX_LENGHT);
+        //console.log($scope.message.length);
+        return $scope.MAX_LENGHT - $scope.message.length;
+    };
+    $scope.hasValidLength = function(){
+        console.log($scope.remaining);
+        if(this.remaining > 0)
+        {
+            console.log(true);
+            return true;
+        }
+        else
+        {
+            console.log(false);
+            return false;
+        }
+    };
+    
+    
+    
+
+    // $scope.test.remaining = function(){
+    //     alert(MAX_LENGHT);
+    //     alert($scope.test.message.length);
+    // 	return $scope.MAX_LENGHT - $scope.test.message.length;
+    // };
+
+    // $scope.test.hasValidLength = function(){
+    // 	if($scope.remaining() > 0)
+    // 	{
+    // 		return true;
+    // 	}
+    // 	else
+    // 	{
+    // 		return false;
+    // 	}
+    // };
+});
